05f0867936f04ff444559290d41c9bcd71b26384
Pablo Lucas <pablo.lucv@gmail.com>
2016-09-13 21:56:00 -0500

    Added basic router

--- public/index.php
+++ public/index.php
@@ -5,3 +5,25 @@
  *
  * PHP version 5.6
  */
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+
+// Add the routes
+$router->add('', ['controller' => 'HomeController', 'action' => 'index']);
+$router->add('posts', ['controller' => 'PostsController', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'PostsController', 'action' => 'new']);
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+        var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL {$url}";
+}
